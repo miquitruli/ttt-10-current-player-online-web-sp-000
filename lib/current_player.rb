0410770c0778e468d6207e2c2e0cfb0aa4f0ff00
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-03 23:41:23 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,7 @@
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    if turn == "X" || index == "O"
+    if turn == "X" || turn == "O"
       counter += 1
       puts "#{counter}"
     end
