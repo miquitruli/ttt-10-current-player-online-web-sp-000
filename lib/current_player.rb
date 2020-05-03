e8c05d5b4740accb31d7fae60a71e8bd214928ec
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-03 23:36:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   board.each do
     counter = 0
-    if board == "X" || "O"
+    if board = "X" || "O"
       counter += 1
       return counter
     else
